df12d47b9077c36068039884a3c7a0de3c0f6e7f
Nicolas Glondu <ngl@zengularity.com>
2017-02-23 16:33:00 +0100

    clenaup

--- scripts/popup.js
+++ scripts/popup.js
@@ -13,16 +13,7 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
 function onWindowLoad() {
 
   message = document.querySelector('#message');
-/*
-  chrome.tabs.executeScript(null, {
-    file: "scripts/getPageSource.js"
-  }, function() {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-    }
-  });
-*/
+
 }
 
 window.onload = onWindowLoad;
